6003a2851b465341d348146403ddbaf04e35da7e
tsitle <technisandk@gmail.com>
2026-01-22 20:39:40 +0100

    Main: add option for displaying version info

--- src/main/java/org/tsitle/demo_cli_app_critical_path/Main.java
+++ src/main/java/org/tsitle/demo_cli_app_critical_path/Main.java
@@ -13,12 +13,17 @@ import java.io.*;
  */
 @CommandLine.Command(
 		name = "cpm",
-		description = "%nCommand line tool for using the Critical Path Method library%n"
+		description = "%nCommand line tool for using the Critical Path Method library%n",
+		versionProvider = Main.ManifestVersionProvider.class
 	)
 public class Main implements Runnable {
 	@SuppressWarnings("unused")
 	@CommandLine.Option(names = {"-h", "--help"}, usageHelp = true, description = "display this help message")
 	private boolean doPrintHelp;
+
+	@SuppressWarnings("unused")
+	@CommandLine.Option(names = {"-V", "--version"}, versionHelp = true, description = "display version info")
+	boolean doPrintVersion;
 
 	@SuppressWarnings("unused")
 	@CommandLine.Option(names = {"--output-html"}, description = "optional: write results to an HTML file")
@@ -27,6 +32,8 @@ public class Main implements Runnable {
 	@SuppressWarnings("unused")
 	@CommandLine.Parameters(description = "path to the JSON configuration file (may be prefixed with 'rsc:')")
 	private String configJsonFilename;
+
+	private final static String APP_NAME = "cpm_demo";
 
 	public static void main(String[] args) {
 		int resI = new CommandLine(new Main())
@@ -71,6 +78,38 @@ public class Main implements Runnable {
 			return cmd.getExitCodeExceptionMapper() != null
 					? cmd.getExitCodeExceptionMapper().getExitCode(ex)
 					: cmd.getCommandSpec().exitCodeOnExecutionException();
+		}
+	}
+
+	/**
+	 * {@link CommandLine.IVersionProvider} implementation that returns version information from the {@code /META-INF/MANIFEST.MF} file.
+	 */
+	static class ManifestVersionProvider implements CommandLine.IVersionProvider {
+		public String[] getVersion() throws Exception {
+			java.util.Enumeration<java.net.URL> resources = Main.class.getClassLoader().getResources("META-INF/MANIFEST.MF");
+			while (resources.hasMoreElements()) {
+				java.net.URL url = resources.nextElement();
+				try {
+					java.util.jar.Manifest manifest = new java.util.jar.Manifest(url.openStream());
+					if (isApplicableManifest(manifest)) {
+						java.util.jar.Attributes attr = manifest.getMainAttributes();
+						return new String[] { get(attr, "Implementation-Title") + " version \"" +
+								get(attr, "Implementation-Version") + "\"" };
+					}
+				} catch (IOException ex) {
+					return new String[] { "Unable to read from " + url + ": " + ex };
+				}
+			}
+			return new String[0];
+		}
+
+		private boolean isApplicableManifest(java.util.jar.Manifest manifest) {
+			java.util.jar.Attributes attributes = manifest.getMainAttributes();
+			return Main.APP_NAME.equals(get(attributes, "Implementation-Title"));
+		}
+
+		private static Object get(java.util.jar.Attributes attributes, String key) {
+			return attributes.get(new java.util.jar.Attributes.Name(key));
 		}
 	}
 }
