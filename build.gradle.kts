fc710f70702fda0b24377b9cb4c0ea8455014542
tsitle <technisandk@gmail.com>
2026-01-24 14:47:06 +0100

    build.gradle.kts: add OS and ARCH to output filenames

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,60 @@ plugins {
 	id("java")
 	id("application")
 	id("org.beryx.jlink") version "3.2.0"
+	id("com.google.osdetector") version "1.7.3"  // see https://github.com/google/osdetector-gradle-plugin
 }
 
 group = "org.tsitle.demo_cli_app_critical_path"
 version = "1.0"
 
-val APP_NAME = "cpm_demo"
+val propProjName = "cpm_demo"
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+fun getOperatingSystemName() : String {
+	return if (org.gradle.internal.os.OperatingSystem.current().isMacOsX) {
+		"macos"
+	} else if (org.gradle.internal.os.OperatingSystem.current().isLinux) {
+		"linux"
+	} else if (org.gradle.internal.os.OperatingSystem.current().isWindows) {
+		"win"
+	} else {
+		throw Error("Operating System not supported")
+	}
+}
+
+fun getCpuArchitecture() : String {
+	return when (System.getProperty("os.arch")) {
+		"x86_64", "x64", "amd64" -> "x64"
+		/*Linux/macOS:*/"aarch64" -> "aarch64"
+		else -> throw Error("CPU Architecture not supported")
+	}
+}
+
+fun getLinuxDistroType() : String {
+	if (getOperatingSystemName() != "linux") {
+		return "none"
+	}
+	val tmpRel = osdetector.release
+	if (tmpRel.isLike("debian")) {
+		return "debian"
+	}
+	if (tmpRel.isLike("redhat") || tmpRel.isLike("fedora")) {
+		return "redhat"
+	}
+	throw Error("Linux distribution type '${tmpRel}' not supported")
+}
+
+val osName: String = getOperatingSystemName()
+val cpuArch: String = getCpuArchitecture()
+if (osName == "win" && cpuArch != "x64") {
+	throw Error("Cannot build for ${osName}-${cpuArch}")
+}
+val lxDistroType: String = getLinuxDistroType()
+
+println("Host: ${osName}-${cpuArch}")
+
+// ---------------------------------------------------------------------------------------------------------------------
 
 repositories {
 	mavenCentral()
@@ -40,7 +88,8 @@ application {
 tasks.jar {
 	manifest {
 		attributes["Main-Class"] = "org.tsitle.demo_cli_app_critical_path.Main"
-		attributes["Implementation-Title"] = APP_NAME
+		// the following two attributes are required in order for the '--version' CLI option to work
+		attributes["Implementation-Title"] = propProjName
 		attributes["Implementation-Version"] = version
 	}
 }
@@ -48,12 +97,12 @@ tasks.jar {
 jlink {
 	// name of the launcher script produced in the launcher image
 	launcher {
-		name = APP_NAME
+		name = propProjName
 	}
 
 	// output directory and ZIP filename for the launcher image
-	imageDir = File(layout.buildDirectory.get().toString(), "${APP_NAME}-${version}")
-	imageZip = File(layout.buildDirectory.get().toString(), "${APP_NAME}-${version}.zip")
+	imageDir = File(layout.buildDirectory.get().toString(), "${propProjName}-${osName}-${cpuArch}-${version}")
+	imageZip = File(layout.buildDirectory.get().toString(), "${propProjName}-${osName}-${cpuArch}-${version}.zip")
 
 	// reduce the size of the launcher image
 	options.set(listOf("--strip-debug", "--compress", "zip-6", "--no-header-files", "--no-man-pages"))
